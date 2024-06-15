1ed8236b070169f0c67f2a69908b40607c70889d
Parth Lad <81832445+ladparth@users.noreply.github.com>
2024-06-15 16:08:34 -0400

    Update URLs in sitemap and social share components (#15)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,14 +4,14 @@ export default async function sitemap() {
   const posts = await getPosts();
 
   const blogs = posts.map((post) => ({
-    url: `${process.env.BASE_URL}/blog/${post.slug}`,
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.slug}`,
     lastModified: new Date(post.updatedAt).toISOString(),
     changeFrequency: "daily",
     priority: 0.8,
   }));
 
   const routes = ["", "/about", "/blog"].map((route) => ({
-    url: `${process.env.BASE_URL}${route}`,
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}${route}`,
     lastModified: new Date().toISOString(),
     changeFrequency: "always",
     priority: 1,
