d63d6362b95b047c3a73fc7da140b8778e4cfaea
Parth Lad <81832445+ladparth@users.noreply.github.com>
2024-06-27 19:21:37 -0400

    Update Adsense script loading strategy to lazyOnload (#27)

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -7,7 +7,7 @@ export default function Adsense() {
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process
         .env.NEXT_PUBLIC_ADSENSE_PUB_ID!}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
+      strategy="lazyOnload"
     ></Script>
   );
 }
